5332c1f1bef063a43b86e09097dd926238508a52
stan <stan@192.168.1.11>
2020-11-16 15:37:22 +0100

    debug test fix : test now working

--- Senettis/src/main/java/testsConnexion/TestProduit.java
+++ Senettis/src/main/java/testsConnexion/TestProduit.java
@@ -4,10 +4,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import classes.Product;
+import classes.Status;
 
 public class TestProduit {
 	public static void main(String[] args) throws SQLException {
-		Product p = new Product("Dr Papper", 3.5, "Coca à l'amande", "Publié");
+		Product p = new Product("Dr Papper", 3.5, "Coca à l'amande", Status.PUBLISHED);
 		p.insertDatabase();
 		// p.deleteDatabase(1);
 
